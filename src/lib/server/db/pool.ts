fda3dde1ff310cc40c6f4090a907100a296f138b
GoldRushEnergy <alexander@goldrushenergy.com>
2023-12-13 18:35:36 -0800

    Update database connection to use Heroku DATABASE_URL

--- src/lib/server/db/pool.ts
+++ src/lib/server/db/pool.ts
@@ -3,11 +3,13 @@ import { env } from '$env/dynamic/private'; // Import the env from SvelteKit's e
 import pkg from 'pg';
 const { Pool } = pkg;
 
-// Create a new pool instance with your database connection details
+
+// Heroku provides DATABASE_URL, a complete connection string
+const connectionString = env.DATABASE_URL;
+
 export const pool = new Pool({
-	user: env.DB_USER,
-	host: env.DB_HOST,
-	database: env.DB_NAME,
-	password: env.DB_PASSWORD,
-	port: parseInt(env.DB_PORT || '5432', 10)
+	connectionString: connectionString,
+	ssl: {
+		rejectUnauthorized: false // Required for Heroku
+	}
 });
